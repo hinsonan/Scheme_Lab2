75f2801751ef54d5389ab2502971e45eb4c03a79
Andrew Hinson <hinsonan@gmail.com>
2017-09-07 22:33:50 -0500

    Answers for 1-6

--- CIS470_Lab2_Hinson.rkt
+++ CIS470_Lab2_Hinson.rkt
@@ -2,8 +2,63 @@
 
 ;1
 (define(circle-area r)
-  (* 3.14 (* r r))
+  (* 3.14159 (* r r))
   )
 (circle-area 2)
 
+(newline)
+
 ;2
+(define(miles-to-nautical x)
+  (* x 1.15)
+
+)
+
+(define(mph-to-knots x)
+  (* x 1.15077945)
+)
+
+(miles-to-nautical 100)
+
+(mph-to-knots 100)
+
+(newline)
+
+;3
+(define(kilometers-to-nautical x)
+  (* 1.6093 x)
+)
+(kilometers-to-nautical 100)
+
+(newline)
+
+;4
+(define (hypotenuse x y)
+  (sqrt(+ (* x x) (* y y)))
+  )
+(hypotenuse 3 4)
+
+;5
+(define(F-TO-C x)
+ (- (* (+ x 40) (/ 5 9)) 40)
+  )
+
+(define(C-TO-F x)
+ (- (* (+ x 40) (/ 9 5)) 40)
+  )
+
+(F-TO-C 20)
+(C-TO-F 20)
+
+(newline)
+;6
+(define (reverse-digits n)
+  (let loop ((n n) (r 0))
+    (if (zero? n) r
+        (loop (quotient n 10) (+ (* r 10) (remainder n 10))))))
+
+(reverse-digits 345)
+
+(newline)
+
+;7
